a1483a38330925330887138a83bdd8a997cb7e82
Ondrej <info@anzui.de>
2011-05-26 22:49:28 +0200

    - generating streets now working - started z-sorting, needs a real algorithm (how does osm renderers do it?) - different object types are now distributed to different layers - bug to fix: import get's slower the longer it takes. Seems like, the more objects we have in the scene the longer everything takes. Maybe every X meshes of the same osm type should be grouped into one object?

--- import_osm.py
+++ import_osm.py
@@ -1,4 +1,6 @@
 import bpy
+import math
+from mathutils import geometry
 from mathutils import Vector
 from xml.dom.minidom import parse,parseString
 from .helpers import Debugger,Profiler
@@ -7,9 +9,12 @@ BUILDING_TAG = 'building'
 STREET_TAG = 'highway'
 AREA_TAGS = ('area','natural','landuse','leisure')
 RAILWAY_TAG = 'railway'
-LANE_WIDTH = 3
-DEFAULT_BUILDING_HEIGHT = 10
+LANE_WIDTH = 3.5
+DEFAULT_BUILDING_HEIGHT = 15
 UNIT_SCALES = {'m':1,'ft':0.305}
+OFFSET_STEP = 0.01
+OFFSET_ORDER = ['natural','landuse','leisure','street','railway','land','sand','beach','grassland','grass','coastline','water']
+LAYERS = ['building','area','street',None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None]
 
 EQUATOR_RADIUS = 6378137        # greatest earth radius (equator)
 POLE_RADIUS = 6356752.314245    # smallest earth radius (pole)
@@ -64,6 +69,21 @@ def selectObject(obj):
     if profile:
         profiler.end('selectObject')
 
+def setOnLayer(obj,layer):
+    for i in range(0,20):
+        if i == layer:
+            obj.layers[i] = True
+        else:
+            obj.layers[i] = False
+
+def editMode(mode=True):
+    if mode:
+        if bpy.context.scene.objects.active:
+            bpy.ops.object.mode_set(mode='EDIT') #Operators
+    else:
+        if bpy.context.scene.objects.active:
+            bpy.ops.object.mode_set(mode='OBJECT') #Operators
+
 def selectMesh():
     if profile:
         profiler.start('selectMesh')
@@ -74,6 +94,20 @@ def selectMesh():
 
     if profile:
         profiler.end('selectMesh')
+
+def selectCurve():
+    if profile:
+        profiler.start('selectCurve')
+
+    if bpy.context.scene.objects.active:
+        bpy.ops.object.mode_set(mode='EDIT') #Operators
+        bpy.ops.curve.select_all(action='SELECT')#select all the face/vertex/edge
+
+    if profile:
+        profiler.end('selectCurve')
+
+def deselectCurve():
+    deselectMesh()
 
 def deselectMesh():
     if profile:
@@ -135,11 +169,27 @@ class OSM():
     ways = {}
     relations = {}
     bounds = (Vector((0.0,0.0)),Vector((0.0,0.0)))
+    dimensions = Vector((0.0,0.0))
     version = ''
     generator = ''
     generate_process = 0.0
+    ground = None
+    camera = None
+    sub_offsets = {}
 
     def __init__(self,xml):
+        self.nodes = {}
+        self.ways = {}
+        self.relations = {}
+        self.bounds = (Vector((0.0,0.0)),Vector((0.0,0.0)))
+        self.dimensions = Vector((0.0,0.0))
+        self.version = ''
+        self.generator = ''
+        self.generate_process = 0.0
+        self.ground = None
+        self.camera = None
+        self.sub_offsets = {}
+
         self.xml = xml
         self.version = xml.attributes['version'].value
         self.generator = xml.attributes['generator'].value
@@ -155,6 +205,9 @@ class OSM():
         self.bounds[1][0] = co[0]
         self.bounds[1][1] = co[1]
 
+        self.dimensions[0] = self.bounds[1][0]-self.bounds[0][0]
+        self.dimensions[1] = self.bounds[1][1]-self.bounds[0][1]
+
     def generate(self):
         if profile:
             profiler.start('OSM.generate')
@@ -164,10 +217,16 @@ class OSM():
 
         deselectObjects()
 
+        self.createGround()
+        self.createCamera()
+
         process_step = 100/len(self.ways)
         
         for id in self.ways:
             self.ways[id].generate()
+            if self.ways[id].object:
+                self.setOffset(self.ways[id])
+                self.setLayer(self.ways[id])
             self.generate_process+=process_step
 
         update()
@@ -176,6 +235,53 @@ class OSM():
             debugger.debug("OSM import complete!")
         if profile:
             profiler.end('OSM.generate')
+
+    def createGround(self):
+        mesh = bpy.data.meshes.new("Ground")
+        self.ground = bpy.data.objects.new("Ground",mesh)
+        bpy.context.scene.objects.link(self.ground)
+
+        setOnLayer(self.ground,0)
+
+        mesh.vertices.add(4)
+        mesh.vertices[0].co = Vector((0.0,0.0,0.0))
+        mesh.vertices[1].co = Vector((self.dimensions[0],0.0,0.0))
+        mesh.vertices[2].co = Vector((self.dimensions[0],self.dimensions[1],0.0))
+        mesh.vertices[3].co = Vector((0.0,self.dimensions[1],0.0))
+        
+        selectObject(self.ground)
+        selectMesh()
+        
+        bpy.ops.mesh.edge_face_add()
+        bpy.ops.uv.unwrap()
+
+        deselectMesh()
+
+        # Material
+        bpy.ops.object.material_slot_add()
+        if 'ground' in bpy.data.materials:
+            self.ground.material_slots[0].material = bpy.data.materials["ground"]
+
+        deselectObject(self.ground)
+
+    def createCamera(self):
+        angle = 60.0;
+        cam = bpy.data.cameras.new("OSMCamera")
+        self.camera = bpy.data.objects.new("OSMCamera",cam)
+        bpy.context.scene.objects.link(self.camera)
+
+        setOnLayer(self.camera,0)
+
+        self.camera.rotation_euler = Vector((math.radians(angle),0.0,0.0))
+        self.camera.data.type = 'ORTHO'
+        self.camera.data.clip_end = 40000.0 # 40 km
+        self.camera.data.clip_start = 0.0
+        self.camera.data.lens_unit = 'DEGREES'
+        self.camera.data.lens = 90
+        self.camera.data.ortho_scale = self.dimensions[0]
+        self.camera.location = Vector((self.dimensions[0]/2,self.dimensions[1]/(2+(90/angle)),100))
+
+        bpy.context.scene.camera = self.camera
 
     def getNodes(self,xml):
         if profile:
@@ -271,6 +377,22 @@ class OSM():
 
         return co
 
+    def setOffset(self,way):
+        for i in range(0,len(OFFSET_ORDER)):
+            if OFFSET_ORDER[i]==way.type[0] or OFFSET_ORDER[i]==way.type[1] or OFFSET_ORDER[i]==way.type[2]:
+                way.object.location[2] = OFFSET_STEP*(i+1)
+                return
+
+    def setLayer(self,way):
+        for i in range(0,20):
+            if way.type[0]==LAYERS[i]:
+                way.object.layers[i] = True
+            else:
+                way.object.layers[i] = False
+
+        if LAYERS[0]!=way.type:
+            way.object.layers[0] = False
+
 class Tag():
     name = None
     value = None
@@ -326,6 +448,8 @@ class Way():
             self.type[1] = self.tags[STREET_TAG].value
             if 'lanes' in self.tags:
                 self.width = LANE_WIDTH*float(self.tags['lanes'].value)
+            else:
+                self.width = LANE_WIDTH
 
         if profile:
             profiler.end('Way.setType')
@@ -351,8 +475,8 @@ class Way():
                 debugger.debug('%3.2f' % (self.osm.generate_process) +'% ' + self.name)
             self.createObject()
             selectObject(self.object)
-            self.setMaterial()
             self.create()
+            self.setMaterial()
             deselectObject(self.object)
 
         if profile:
@@ -368,6 +492,10 @@ class Way():
             self.createArea()
         elif self.type[0]=='street':
             self.createStreet()
+
+        self.object.data.use_auto_smooth = True
+        #self.object.data.show_double_sided = False
+        bpy.ops.object.shade_smooth()
 
         if profile:
             profiler.end('Way.create')
@@ -424,6 +552,8 @@ class Way():
         if profile:
             profiler.end('mesh.extrude')
 
+        selectMesh()
+
         if profile:
             profiler.start('remove_doubles')
         bpy.ops.mesh.remove_doubles()
@@ -466,7 +596,15 @@ class Way():
         bpy.ops.mesh.normals_make_consistent()
         if profile:
             profiler.end('mesh.normals')
-            
+
+#        selectMesh()
+#
+#        if profile:
+#            profiler.start('mesh.unwrap')
+#        bpy.ops.uv.unwrap()
+#        if profile:
+#            profiler.end('mesh.unwrap')
+
         deselectMesh()
         bpy.ops.object.origin_set(type="ORIGIN_GEOMETRY")
 
@@ -478,36 +616,66 @@ class Way():
             profiler.start('Way.createStreet')
 
         self.createEdges()
+        bpy.ops.object.origin_set(type="ORIGIN_GEOMETRY")
+
+        bpy.ops.object.convert(target="CURVE")
+
+        # uv mapping
+        bpy.ops.object.data.use_uv_as_generated = True
+
+        #self.object.rotation_euler[1] = math.radians(90)
+#        selectCurve()
+#        bpy.ops.transform.create_orientation(use=True)
+#        bpy.ops.transform.rotate(value=[math.radians(-90)],axis=(0.0,1.0,0.0))
+#
+#        bpy.ops.curve.subdivide()
+#
+#        deselectCurve()
+
+        self.object.data.extrude = 0.00001
+
+        bpy.ops.object.modifier_add(type="SOLIDIFY")
+        solidify = self.object.modifiers[0]
+        solidify.thickness = self.width
+        solidify.offset = 0
+        
+        bpy.ops.object.convert(target="MESH")
+
         selectMesh()
-        # TODO: extrude edges to left and right by self.width/2
-            
+#        bpy.ops.mesh.subdivide(smoothness=1)
+#        deselectMesh()
+        
+        if profile:
+            profiler.start('remove_doubles')
+        bpy.ops.mesh.remove_doubles()
+        if profile:
+            profiler.end('remove_doubles')
+
         if profile:
             profiler.start('mesh.normals')
-        bpy.ops.mesh.normals_make_consistent()
+        bpy.ops.mesh.normals_make_consistent(inside=True)
         if profile:
             profiler.end('mesh.normals')
-            
+
         deselectMesh()
-        bpy.ops.object.origin_set(type="ORIGIN_GEOMETRY")
 
         if profile:
             profiler.end('Way.createStreet')
-
+        
     def setMaterial(self):
         if profile:
             profiler.start('Way.setMaterial')
             
         bpy.ops.object.material_slot_add()
-        if self.type[2] and bpy.data.materials.get(self.type[2]):
+        if self.type[2] and (self.type[2] in bpy.data.materials):
             self.object.material_slots[0].material = bpy.data.materials[self.type[2]]
-        elif self.type[1] and bpy.data.materials.get(self.type[1]):
+        elif self.type[1] and (self.type[1] in bpy.data.materials):
             self.object.material_slots[0].material = bpy.data.materials[self.type[1]]
-        elif self.type[0] and bpy.data.materials.get(self.type[0]):
+        elif self.type[0] and (self.type[0] in bpy.data.materials):
             self.object.material_slots[0].material = bpy.data.materials[self.type[0]]
 
         if profile:
             profiler.end('Way.setMaterial')
-
 
 class Node():
     id = None
